655bea0f684acdd9a3a768544b096c167a32cf48
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-06-29 19:25:00 +0100

    simplified storefinder widget css

--- widgets/storefinder/postcss.config.cjs
+++ widgets/storefinder/postcss.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
                     return selector;
                 }
                 return prefixedSelector;
-            },
-        }),
-    ],
+            }
+        })
+    ]
 };
