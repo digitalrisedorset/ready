d99330872ad9e8ba1e5641b6a3e309c9458c2f38
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-07-22 01:41:45 +0100

    Synchronize shared resources with latest widget conventions

--- packages/widget-build/shared-resources/vite_project/src/mountWidget.tsx
+++ packages/widget-build/shared-resources/vite_project/src/mountWidget.tsx
@@ -1,13 +1,12 @@
 import {createRoot} from "react-dom/client";
-import React from "react";
 import {WidgetWrapper} from "./WidgetWrapper.tsx";
 import {getMountedHost} from "./lib/hostReader.ts";
 import {ActivityContextProvider} from "./activity/Context/ActivityContextProvider.tsx";
 
-export async function mountWidget(hostElement: HTMLElement, config: unknown) {
+export async function mountWidget(hostElement: HTMLElement, rawConfig: unknown) {
     const mountedHost = getMountedHost(hostElement);
 
     createRoot(mountedHost).render(<ActivityContextProvider hostElement={hostElement}>
-        <WidgetWrapper rawConfig={config} />
+        <WidgetWrapper rawConfig={rawConfig} />
     </ActivityContextProvider>);
 }
